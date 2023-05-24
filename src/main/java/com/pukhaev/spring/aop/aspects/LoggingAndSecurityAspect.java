9853c271238d7e41f145ffdd3c393766d04636b7
vikpuhaev <vik.puhaev@gmail.com>
2023-05-24 13:33:17 +0200

    Lesson 29 done

--- src/main/java/com/pukhaev/spring/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/com/pukhaev/spring/aop/aspects/LoggingAndSecurityAspect.java
@@ -9,9 +9,48 @@ import org.springframework.stereotype.Component;
 @Aspect  // Аспект класс отвечающий за сквозную функциональность
 public class LoggingAndSecurityAspect {
 
+    @Pointcut("execution(* com.pukhaev.spring.aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary(){}
+
+    @Pointcut("execution(public void com.pukhaev.spring.aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary(){}
+
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+    private void allMethodsExceptReturnFromUniLibrary(){}
+
+    @Before("allMethodsExceptReturnFromUniLibrary()")
+    public void beforeAllMethodsExceptReturnAdvice() {
+        System.out.println("beforeAllMethodsExceptReturnAdvice: writing Log #10");
+    }
+
+//    @Pointcut("execution(* com.pukhaev.spring.aop.UniLibrary.get* ())")
+//    private void allGetMethodsFromUniLibrary(){}
+//
+//    @Pointcut("execution(* com.pukhaev.spring.aop.UniLibrary.return* ())")
+//    private void allReturnMethodsFromUniLibrary(){}
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary(){}
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice(){
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
+//    }
+
+
     // Объявление Pointcut
-    @Pointcut("execution(* get*())")
-    private void allGetMethods(){}
+//    @Pointcut("execution(* get*())")
+//    private void allGetMethods(){}
 
     // Pointcut -- ("execution(...)") показывает где и когда должен быть применен Advice
     // показывает когда должен выполниться сквозной код. Если не указано из какого класса метод, то метод будет вызываться для всех классов
@@ -19,15 +58,15 @@ public class LoggingAndSecurityAspect {
     // execution(public void get*() -- логирование будет по всем методам с get во всех классах
     // когда стоит * это соответствует "любой"
     // когда в параметрах метода стоит *, то это любой ОДИН параметр, если нужно несколько любых параметров и/или без параметров ставим ".." (две точки)
-    @Before("allGetMethods()")
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetLoggingAdvice: Попытка получить книгу / журнал");
-    }
-
-    @Before("allGetMethods()")
-    public void beforeGetSecurityAdvice(){
-        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги / журнала");
-    }
+//    @Before("allGetMethods()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: Попытка получить книгу / журнал");
+//    }
+//
+//    @Before("allGetMethods()")
+//    public void beforeGetSecurityAdvice(){
+//        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги / журнала");
+//    }
 
     // Если нам не важен тип возврщаемых данных то можно заменть void на *
     // Access modifier не обязателен, поэтому его можно удалить или заменить на * и будет подходить метод с любыми правами доступа
