bd33efba8e72871a9bce9dcac8336e98eddb96ee
vikpuhaev <vik.puhaev@gmail.com>
2023-05-26 16:54:11 +0200

    Part 2 completed. Lesson 36 done.

--- src/main/java/com/pukhaev/spring/aop/aspects/NewLoggingAspect.java
+++ src/main/java/com/pukhaev/spring/aop/aspects/NewLoggingAspect.java
@@ -13,12 +13,18 @@ public class NewLoggingAspect {
         System.out.println("aroundReturnBookLoggingAspect: в библиотеку пытаются вернуть книгу");
         // вызов таргет метода. В advice @Around нужно самостоятельно вызывать таргет метод через proceedingJoinPoint.proceed()
         // .proceed возращет Object
-        long begin = System.currentTimeMillis();
-        Object targetMethodResult = proceedingJoinPoint.proceed();
-//        targetMethodResult = "Преступление и Наказание";
-        long end = System.currentTimeMillis();
+        Object targetMethodResult = null;
+        try {
+            targetMethodResult = proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAspect: было поймано исключение " + e);
+            // Переназначение таргет метода
+//            targetMethodResult = "Неизвестное название книги";
+
+            // пробрасывание исключения дальше
+            throw e;
+        }
         System.out.println("aroundReturnBookLoggingAspect: в библиотеку успешно вернули книгу");
-        System.out.println("aroundReturnBookLoggingAspect: метод returnBook выполнил работу за " + (end - begin) + "мс");
         return targetMethodResult;
     }
 }
